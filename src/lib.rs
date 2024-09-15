08c660a8b4e5502f91e039ae383a00920a0b639b
Sulaiman Ghori <sulaiman.ghori@outlook.com>
2024-09-14 18:03:42 -0700

    :fire: (Python) Remove python packaging in favor of web demo
    Could probably make both work but easier to remove right now, will decide if keep python package in future

--- src/lib.rs
+++ src/lib.rs
@@ -6,16 +6,16 @@ pub mod parsing_utils;
 pub mod rendering;
 pub mod types;
 
-use crate::parse_and_render::render_to_file;
-use crate::types::StarCatalogArgs;
+// use crate::parse_and_render::render_to_file;
+// use crate::types::StarCatalogArgs;
 
-use pyo3::prelude::*;
+// use pyo3::prelude::*;
 
-#[pyfunction]
-fn render_to_file_py(args: StarCatalogArgs) -> PyResult<()> {
-    render_to_file(&args)
-        .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))
-}
+// #[pyfunction]
+// fn render_to_file_py(args: StarCatalogArgs) -> PyResult<()> {
+//     render_to_file(&args)
+//         .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))
+// }
 
 // #[pyfunction]
 // fn read_and_render_py(args: StarCatalogArgs) -> PyResult<ImageBuffer<Rgb<u8>, Vec<u8>>> {
@@ -23,12 +23,12 @@ fn render_to_file_py(args: StarCatalogArgs) -> PyResult<()> {
 //         .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))
 // }
 
-/// A Python module implemented in Rust. The name of this function must match
-/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
-/// import the module.
-#[pymodule]
-fn starfinder(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_class::<StarCatalogArgs>()?;
-    m.add_function(wrap_pyfunction!(render_to_file_py, m)?)?;
-    Ok(())
-}
+// A Python module implemented in Rust. The name of this function must match
+// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+// import the module.
+// #[pymodule]
+// fn starfinder(m: &Bound<'_, PyModule>) -> PyResult<()> {
+//     m.add_class::<StarCatalogArgs>()?;
+//     m.add_function(wrap_pyfunction!(render_to_file_py, m)?)?;
+//     Ok(())
+// }
--- src/types/args.rs
+++ src/types/args.rs
@@ -1,69 +1,66 @@
 use clap::Parser;
-use pyo3::prelude::{pyclass, pymethods};
+// use pyo3::prelude::{pyclass, pymethods};
 
-#[pyclass]
+// #[pyclass]
 #[derive(Parser, Debug, Clone)]
 pub struct StarCatalogArgs {
     // #[pyo3(get, set)]
-    // pub source: String,
-    #[pyo3(get, set)]
     pub center_ra: f64,
-    #[pyo3(get, set)]
+    // #[pyo3(get, set)]
     pub center_dec: f64,
-    #[pyo3(get, set)]
+    // #[pyo3(get, set)]
     pub fov_w: f64,
-    #[pyo3(get, set)]
+    // #[pyo3(get, set)]
     pub fov_h: f64,
-    #[pyo3(get, set)]
+    // #[pyo3(get, set)]
     pub roll: f64,
-    #[pyo3(get, set)]
+    // #[pyo3(get, set)]
     pub fov_max: f64,
-    #[pyo3(get, set)]
+    // #[pyo3(get, set)]
     pub max_magnitude: f64,
-    #[pyo3(get, set)]
+    // #[pyo3(get, set)]
     pub lambda_nm: f64,
-    #[pyo3(get, set)]
+    // #[pyo3(get, set)]
     pub pixel_size_m: f64,
-    #[pyo3(get, set)]
+    // #[pyo3(get, set)]
     pub width: u32,
-    #[pyo3(get, set)]
+    // #[pyo3(get, set)]
     pub height: u32,
-    #[pyo3(get, set)]
+    // #[pyo3(get, set)]
     pub output: String,
 }
 
-#[pymethods]
-impl StarCatalogArgs {
-    #[new]
-    fn new(
-        // source: String,
-        center_ra: f64,
-        center_dec: f64,
-        fov_w: f64,
-        fov_h: f64,
-        roll: f64,
-        fov_max: f64,
-        max_magnitude: f64,
-        lambda_nm: f64,
-        pixel_size_m: f64,
-        width: u32,
-        height: u32,
-        output: String,
-    ) -> Self {
-        Self {
-            // source,
-            center_ra,
-            center_dec,
-            fov_w,
-            fov_h,
-            roll,
-            fov_max,
-            max_magnitude,
-            lambda_nm,
-            pixel_size_m,
-            width,
-            height,
-            output,
-        }
-    }
-}
+// #[pymethods]
+// impl StarCatalogArgs {
+//     // #[new]
+//     fn new(
+//         center_ra: f64,
+//         center_dec: f64,
+//         fov_w: f64,
+//         fov_h: f64,
+//         roll: f64,
+//         fov_max: f64,
+//         max_magnitude: f64,
+//         lambda_nm: f64,
+//         pixel_size_m: f64,
+//         width: u32,
+//         height: u32,
+//         output: String,
+//     ) -> Self {
+//         Self {
+//             // source,
+//             center_ra,
+//             center_dec,
+//             fov_w,
+//             fov_h,
+//             roll,
+//             fov_max,
+//             max_magnitude,
+//             lambda_nm,
+//             pixel_size_m,
+//             width,
+//             height,
+//             output,
+//         }
+//     }
+// }
